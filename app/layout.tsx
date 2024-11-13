e16731044a5533343cea9e63fccac89b7cb83102
rakess70 <rakess70@gmail.com>
2024-11-13 05:04:59 -0500

    adjust layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,33 @@
-// app/layout.tsx
+// app/layout.ts
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import WalletProviderWrapper from "./WalletProviderWrapper";
-import ConnectButton from "./components/ConnectButton";  // Import ConnectButton
+import ConnectButton from "./components/ConnectButton";
+import { fetchCandyMachineData } from "./lib/metaplexService"; // Import the data fetcher
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Number Forty-Seven Minting Button",
-  description: "Crossmint and Affiliate Integrated",
-};
+export async function generateMetadata(): Promise<Metadata> {
+  // Retrieve Candy Machine ID from environment variable
+  const candyMachineId = process.env.NEXT_PUBLIC_SOLANA_CANDY_MACHINE_ID;
+  
+  if (!candyMachineId) {
+    console.warn("Candy Machine ID is not defined in the environment variables.");
+    return {
+      title: "Minting Button",
+      description: "Crossmint and Affiliate Integrated",
+    };
+  }
+
+  const data = await fetchCandyMachineData(candyMachineId);
+
+  return {
+    title: data?.collectionName ? `${data.collectionName} Minting Button` : "Minting Button",
+    description: "Powered by Mintomatic and Referral Radius by Bellwether Online Solutions",
+  };
+}
 
 export default function RootLayout({
   children,
